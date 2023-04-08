765e3f508de0cc1c02c24ee277933d7f56089b62
wintyo <wintyo1024@gmail.com>
2023-04-08 11:38:06 +0900

    privateパッケージを用意する

---
+++ README.md
@@ -0,0 +1,3 @@
+# Private Utils パッケージ
+
+Private な Utils パッケージ
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * 挨拶を標準出力する
+ * @param name - 名前
+ */
+export const hello = (name: string) => {
+  console.log(`hello, ${name}!`);
+};
